b0f2d40c83f5d2478f0e4aaef2e34647e2ef7721
Vladislav Dimitrov <dimitrov.vlad@gmail.com>
2017-10-10 18:23:14 +0300

    simple node server

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const hbs = require("hbs");
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome!'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
